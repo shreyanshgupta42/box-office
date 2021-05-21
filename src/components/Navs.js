b9340e4ac55b0c4f5c8728b41144e852c415cde0
shreyanshgupta42 <57253725+shreyanshgupta42@users.noreply.github.com>
2021-05-22 01:31:02 +0530

    added nav bar and created page components

---
+++ src/components/Navs.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const LINKS = [
+  {
+    to: '/',
+    text: 'Home',
+  },
+  {
+    to: '/starred',
+    text: 'Starred',
+  },
+];
+const Navs = () => {
+  return (
+    <div>
+      <ul>
+        {LINKS.map(item => (
+          <li key={item.to}>
+            <Link to={item.to}>{item.text}</Link>
+          </li>
+        ))}
+        <li>
+          <Link to="/starred">Go to starred page</Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navs;
