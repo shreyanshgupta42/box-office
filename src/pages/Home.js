e56c3ccfee423f2864ee654a58f2566d73d4b315
shreyanshgupta42 <57253725+shreyanshgupta42@users.noreply.github.com>
2021-05-22 02:27:03 +0530

    api connection

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,31 @@
-import React from 'react'
+import React,{useState} from 'react'
 import Mainpagelayout from '../components/Mainpagelayout'
 
 const Home = () => {
+    const [input,setInput]=useState('');
+
+    const onInputChange=(ev)=>{
+        setInput(ev.target.value)    
+    }
+
+    const onSearch=()=>{
+        //https://api.tvmaze.com/search/shows?q=men
+        fetch(`https://api.tvmaze.com/search/shows?q=${input}`).then(r=>r.json()).then(result=>{
+            console.log(result);
+        })
+    }
+     
+    const onKeyDown=(ev)=>{
+        if(ev.keyCode===13){
+            onSearch();
+        }
+    }
+     
+
     return (
         <Mainpagelayout>
-            This is home page
+            <input type="text" onChange={onInputChange} onKeyDown={onKeyDown} value={input} />
+            <button type="button" onClick={onSearch}>Search</button>
         </Mainpagelayout  >
     )
 }
