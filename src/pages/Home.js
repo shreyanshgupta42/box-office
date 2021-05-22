df70598cb9acc185d8891f9fe4358bd669be6717
shreyanshgupta42 <57253725+shreyanshgupta42@users.noreply.github.com>
2021-05-22 23:27:23 +0530

    initial api connections

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,33 +1,61 @@
-import React,{useState} from 'react'
-import Mainpagelayout from '../components/Mainpagelayout'
+import React, { useState } from 'react';
+import Mainpagelayout from '../components/Mainpagelayout';
+import {APIGet} from '../misc/config';
 
 const Home = () => {
-    const [input,setInput]=useState('');
+  const [input, setInput] = useState('');
+  const [results, setResult] = useState(null);
 
-    const onInputChange=(ev)=>{
-        setInput(ev.target.value)    
+  const onInputChange = ev => {
+    setInput(ev.target.value);
+  };
+
+  const onSearch = () => {
+    //https://api.tvmaze.com/search/shows?q=men
+    APIGet(`/search/shows?q=${input}`).then(results => {
+      setResult(results);
+      console.log(results);
+    });
+      
+  };
+
+  const onKeyDown = ev => {
+    if (ev.keyCode === 13) {
+      onSearch();
     }
+  };
 
-    const onSearch=()=>{
-        //https://api.tvmaze.com/search/shows?q=men
-        fetch(`https://api.tvmaze.com/search/shows?q=${input}`).then(r=>r.json()).then(result=>{
-            console.log(result);
-        })
+  const renderResults = () => {
+    if (results && results.length === 0) {
+      return <div>no results</div>;
     }
-     
-    const onKeyDown=(ev)=>{
-        if(ev.keyCode===13){
-            onSearch();
-        }
+    if (results && results.length > 0) {
+      console.log('yes i got here');
+      return (
+        <div>
+          {results.map(item => (
+            <div key={item.show.id}>{item.show.name}</div>
+          ))}
+        </div>
+      );
     }
-     
+    return null;
+  };
 
-    return (
-        <Mainpagelayout>
-            <input type="text" onChange={onInputChange} onKeyDown={onKeyDown} value={input} />
-            <button type="button" onClick={onSearch}>Search</button>
-        </Mainpagelayout  >
-    )
-}
+  return (
+    <Mainpagelayout>
+      <input
+        type="text"
+        onChange={onInputChange}
+        onKeyDown={onKeyDown}
+        value={input}
+      />
+      <button type="button" onClick={onSearch}>
+        Search
+      </button>
+      {renderResults()}
+    </Mainpagelayout>
+  );
+};
 
-export default Home
+export default Home;
